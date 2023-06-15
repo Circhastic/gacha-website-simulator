81a2d4d196aaebfb7f85ea733b36e03ec59ba046
Circhastic <achvillanueva20@gmail.com>
2023-06-15 20:08:55 +0800

    v1.4

--- script.js
+++ script.js
@@ -1,68 +1,191 @@
 const itemsData = `{
-    "⭐⭐⭐": {
+    "threeStar": {
         "item1": {
-            "itemName": "Poop",
-            "imageURL": "discord.com/picture.jpg"
+            "itemName": "Hellu~ Good Morning Anthony",
+            "imageURL": "https://cdn.discordapp.com/attachments/1116314806820020294/1118863101987655810/50A4DA3A-960C-4544-BA2E-2BB43B00AB6C.jpg",
+            "itemRank": "⭐⭐⭐"
         },
         "item2": {
-            "itemName": "Trash",
-            "imageURL": "discord.com/picture.jpg"
+            "itemName": "Soyjak Charles",
+            "imageURL": "https://cdn.discordapp.com/attachments/1116314806820020294/1118865031115845692/IMG_0760.png",
+            "itemRank": "⭐⭐⭐"
         },
         "item3": {
-            "itemName": "Garbage",
-            "imageURL": "discord.com/picture.jpg"
-        }
-
-    },
-    "⭐⭐⭐⭐": {
-        "item1": {
-            "itemName": "Charles",
-            "imageURL": "discord.com/picture.jpg"
-        },
-        "item2": {
-            "itemName": "Encar",
-            "imageURL": "discord.com/picture.jpg"
-        },
-        "item3": {
-            "itemName": "Cedric",
-            "imageURL": "discord.com/picture.jpg"
-        },
-        "item4": {
-            "itemName": "Anthony",
-            "imageURL": "discord.com/picture.jpg"
-        },
-        "item5": {
-            "itemName": "Raniel",
-            "imageURL": "discord.com/picture.jpg"
-        }
-    },
-    "⭐⭐⭐⭐⭐": {
-        "item1": {
-            "itemName": "S Distinguished Gentlemen Cedric",
-            "imageURL": "discord.com/picture.jpg"
-        },
-        "item2": {
-            "itemName": "S Anthony",
-            "imageURL": "https://cdn.discordapp.com/attachments/1116296565095669800/1116299432598708234/IMG_9021.JPG"
-        },
-        "item3":{
-            "itemName": "S Charles",
-            "imageURL": "discord.com/picture.jpg"
+            "itemName": "Disgusted Beastmaster Cedric",
+            "imageURL": "https://media.discordapp.net/attachments/1116314806820020294/1117731995833860177/IMG_0061.jpg?width=418&height=558",
+            "itemRank": "⭐⭐⭐"
         },
         "item4":{
-            "itemName": "S Encar",
-            "imageURL": "discord.com/picture.jpg"
+            "itemName":"Formal Beastmaster Cedric",
+            "itemURL":"https://media.discordapp.net/attachments/1116314806820020294/1117731995397660783/IMG_7862.jpg?width=418&height=558",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item5":{
+            "itemName":"Soaked Beast-like Cedric",
+            "itemURL":"https://media.discordapp.net/attachments/1116314806820020294/1117731995045347369/IMG_5592.jpg?width=418&height=558",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item6":{
+            "itemName":"Magnified Phiz Raniel",
+            "itemURL": "https://cdn.discordapp.com/attachments/1116314806820020294/1117725403616907335/89712826_865947137203065_4661882422202204160_n.png",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item7":{
+            "itemName":"Romantic Date Charles",
+            "itemURL":"https://media.discordapp.net/attachments/1116296565095669800/1116301431562051614/IMG_9202.JPG?width=418&height=558",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item8":{
+            "itemName":"Casual Stuffed Raniel",
+            "itemURL":"https://media.discordapp.net/attachments/1116314806820020294/1117734916323541062/333571882_1612344672572146_56121705649313139_n.jpg?width=326&height=558",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item9":{
+            "itemName":"Listen As One Charles",
+            "itemURL":"https://media.discordapp.net/attachments/1116314806820020294/1117736560645918730/348359220_269657312276760_65446423631188751_n.jpg?width=426&height=558",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item10":{
+            "itemName":"Scenic View Charles",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118784571446673418/58461528_1025044434371367_5703122978837889024_n.jpg",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item11":{
+            "itemName":"Romantic Cedric and Anthony",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118783939369238638/IMG_8091.JPG",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item12":{
+            "itemName":"Birthday Photo Cedric and Raniel",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118783925830033539/IMG_8550.jpg",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item13":{
+            "itemName":"Been Chilln'car",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1117738181958320128/FB_IMG_1686549504418.jpg",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item14":{
+            "itemName":"Bored Elementary Student Julian",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118855945410322432/received_1747086465492344.webp",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item15":{
+            "itemName":"Rollercoaster Enthusiast Julian",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118856079560941598/received_1869875643216265.webp",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item16":{
+            "itemName":"The Smiling Raniel",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118855999797870592/IMG_1561.png",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item17":{
+            "itemName":"Pinipig Enjoyer Anthony",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118856148209119262/550819_388733474497925_1590366974_n.png",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item18":{
+            "itemName":"Karakol Attendant Anthony",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118826410103029760/IMG_1438.jpg",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item19":{
+            "itemName":"Master of the Seas Julian",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118863598186401852/IMG20211209164153.jpg",
+            "itemRank": "⭐⭐⭐"
+        },
+        "item20":{
+            "itemName":"Cult Magician Raniel",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118862834026164264/unknown.png",
+            "itemRank": "⭐⭐⭐"
+        }
+    },
+    "fourStar": {
+        "item1": {
+            "itemName": "God of Thunder Charles",
+            "imageURL": "https://media.discordapp.net/attachments/1116314838185029662/1118864974048133160/Screenshot_2023-06-15_190458.png?width=356&height=613",
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item2": {
+            "itemName": "Chinese Tycoon Julian",
+            "imageURL": "https://media.discordapp.net/attachments/1116314838185029662/1117695229747793990/FB_IMG_1686549509783.jpg?width=558&height=558",
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item3": {
+            "itemName": "Post-Tergeo Studious Cedric", 
+            "imageURL": "https://media.discordapp.net/attachments/1116314838185029662/1117726156846809118/353660078_657385679628123_9156198541325417209_n_1.jpg?width=418&height=558",
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item4": {
+            "itemName": "Shining Radiance Anthony",
+            "imageURL": "https://cdn.discordapp.com/attachments/1116314838185029662/1118855945292873728/5D56E8F5-FA9E-4C8C-B8A1-DCB02FF5C920.jpg",
+            "itemRank": "⭐⭐⭐⭐"
         },
         "item5": {
-            "itemName": "S Raniel",
-            "imageURL": "discord.com/picture.jpg"
+            "itemName": "Stylish Afro Raniel",
+            "imageURL": "https://media.discordapp.net/attachments/1116314838185029662/1117725365469708369/IMG_7509.JPG?width=535&height=558",
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item6":{
+            "itemName": "Dapper Gentleman Cedric",
+            "imageURL": "https://media.discordapp.net/attachments/1116314838185029662/1117727292114866237/IMG_7438.jpg?width=418&height=558",
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item7":{
+            "itemName":"Harbinger of Darkness Raniel",
+            "itemURL":"https://cdn.discordapp.com/attachments/1116314806820020294/1118863027916263475/image.png",
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item8":{
+            "itemName": "Suave Debonair Charles",
+            "itemURL": "https://cdn.discordapp.com/attachments/1116314838185029662/1118784393641734164/27913344_765134310362382_6550325320332270385_o.jpg",
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item9":{
+            "itemName":"Multiversal Observer Julian",
+            "itemURL": "https://cdn.discordapp.com/attachments/1116314806820020294/1118855509731201085/received_310980881251309.jpg" ,
+            "itemRank": "⭐⭐⭐⭐"
+        },
+        "item10":{
+            "itemName":"Forest Sage Anthony",
+            "itemURL": "https://cdn.discordapp.com/attachments/1116314838185029662/1118855793924653056/0ADE2A19-4402-4C2F-AEE1-B418B8BB71A2.jpg",
+            "itemRank": "⭐⭐⭐⭐"
+        }
+    },
+    "fiveStar": {
+        "item1": {
+            "itemName": "Distinguished Gentlemen Cedric",
+            "imageURL": "https://media.discordapp.net/attachments/1116296565095669800/1116299386260033546/IMG_8804.JPG?width=455&height=606",
+            "itemRank": "⭐⭐⭐⭐⭐"
+        },
+        "item2": {
+            "itemName": "Omniscient Reader Anthony",
+            "imageURL": "https://cdn.discordapp.com/attachments/1116296565095669800/1116299432598708234/IMG_9021.JPG",
+            "itemRank": "⭐⭐⭐⭐⭐"
+        },
+        "item3":{
+            "itemName": "Creator of Jiu-jitsu Charles",
+            "imageURL": "https://cdn.discordapp.com/attachments/1116314907726585866/1118868887216324638/IMG_20201230_041141531.jpg",
+            "itemRank": "⭐⭐⭐⭐⭐"
+        },
+        "item4":{
+            "itemName": "Thunder Hammer King Encar",
+            "imageURL": "https://cdn.discordapp.com/attachments/1116314907726585866/1118857581209845780/IMG20211229145539.jpg",
+            "itemRank": "⭐⭐⭐⭐⭐"
+        },
+        "item5": {
+            "itemName": "Celestial Countenance Raniel",
+            "imageURL": "https://media.discordapp.net/attachments/1116314907726585866/1117725304945909780/IMG_20221104_222832.jpg?width=418&height=558",
+            "itemRank": "⭐⭐⭐⭐⭐"
         }
 
     },
-    "⭐⭐⭐⭐⭐⭐": {
+    "sixStar": {
         "item1": {
-            "itemName": "SSS Jhumar",
-            "imageURL": "discord.com/picture.jpg"
+            "itemName": "The Unrivaled Jhumar",
+            "imageURL": "https://media.discordapp.net/attachments/1116296565095669800/1118857797338152970/image.png?width=555&height=613",
+            "itemRank": "⭐⭐⭐⭐⭐⭐"
 
         }
     }
@@ -82,17 +205,6 @@ var randomItemName;
 const itemData = JSON.parse(itemsData);
 document.getElementById("header-tickets").innerHTML = rollTickets;
 
-// TODO rarity = "⭐⭐⭐⭐⭐";
-// items = Object.keys(rarity);
-// randomItemIndex = Math.floor(Math.random() * items.length);
-// console.log(items);
-// console.log(rarity);
-// console.log(randomItemIndex);
-// randomItemName = rarity[items[randomItemIndex]].itemName;
-// console.log(randomItemName);
-
-document.getElementById("banner-img").src = items[1].itemName;
-
 //function for chance per character
 function chance(rollPercent) {
 
@@ -100,11 +212,13 @@ function chance(rollPercent) {
     if (specialPityCount == 90){ // 5 star
         if(Math.floor(Math.random() * 100) + 1 < 76 ){ // 5 star
             randomItemName = itemRoller(5);
-            console.log(rollCounter + " ⭐⭐⭐⭐⭐ " + randomItemName + "| Pity: " + specialPityCount);
+            addToBanner(randomItemName);
+            console.log(rollCounter + " ⭐⭐⭐⭐⭐ " + randomItemName + " | Pity: " + specialPityCount);
         }
         else{ //6 star
             randomItemName = itemRoller(6);
-            console.log(rollCounter + " ⭐⭐⭐⭐⭐⭐ " + randomItemName + "| Pity: " + specialPityCount);
+            addToBanner(randomItemName);
+            console.log(rollCounter + " ⭐⭐⭐⭐⭐⭐ " + randomItemName + " | Pity: " + specialPityCount);
         }
         
         specialPityCount = 0;
@@ -112,34 +226,39 @@ function chance(rollPercent) {
     }   
     else if (fourPityCount % 10 == 0) { // 4 star
         randomItemName = itemRoller(4);
-        console.log(rollCounter + " ⭐⭐⭐⭐ " + randomItemName + "| Pity: " + specialPityCount);
+        addToBanner(randomItemName);
+        console.log(rollCounter + " ⭐⭐⭐⭐ " + randomItemName + " | Pity: " + specialPityCount);
 
         fourPityCount = 0;
     }
     else { // Non guaranteed
         if (rollPercent < 90) { // No. is 1 - 89, 3 star
             randomItemName = itemRoller(3);
-        console.log(rollCounter + " ⭐⭐⭐ " + randomItemName + "| Pity: " + specialPityCount);
+            addToBanner(randomItemName);
+            console.log(rollCounter + " ⭐⭐⭐ " + randomItemName + " | Pity: " + specialPityCount);
 
         }
         else if (rollPercent == 100) { // No. is 100, 5 and 6 star 
             if(Math.floor(Math.random() * 100) + 1 < 81 ){ // 5 star .8 percent
-                randomItemName = itemRoller(6);
-                console.log(rollCounter + " ⭐⭐⭐⭐⭐ " + randomItemName + "| Pity: " + specialPityCount);
+                randomItemName = itemRoller(5);
+                addToBanner(randomItemName);
+                console.log(rollCounter + " ⭐⭐⭐⭐⭐ " + randomItemName + "  | Pity: " + specialPityCount);
 
                 
             }
             else { //6 star .2 percent
                 randomItemName = itemRoller(6);
-                console.log(rollCounter + " ⭐⭐⭐⭐⭐⭐ " + randomItemName + "| Pity: " + specialPityCount);
+                addToBanner(randomItemName);
+                console.log(rollCounter + " ⭐⭐⭐⭐⭐⭐ " + randomItemName + " | Pity: " + specialPityCount);
 
             }
             
             specialPityCount = 0;
         }
-        else{ // No. is 100,4 star
+        else { // No. is 90-99,4 star
             randomItemName = itemRoller(4);
-            console.log(rollCounter + " ⭐⭐⭐⭐ " + randomItemName + "| Pity: " + specialPityCount);
+            addToBanner(randomItemName);
+            console.log(rollCounter + " ⭐⭐⭐⭐ " + randomItemName + " | Pity: " + specialPityCount);
 
             fourPityCount = 0;
         }
@@ -151,12 +270,66 @@ function pullDetail() {
     chance(rollNumber);
 }
 
+
+function createBannerBox(itemName, imageURL, itemRank) {
+    const box = document.createElement("div");
+    box.classList.add("banner-box");
+    box.style.backgroundImage = `url(${imageURL})`;
+    box.style.backgroundSize = "cover";
+    box.style.backgroundRepeat = "no-repeat";
+    box.style.backgroundPosition = "center";
+    box.textContent = itemName + itemRank;
+    return box;
+}
+
+function clearBanner() {
+    const banner = document.getElementById("banner");
+    while (banner.firstChild) {
+      banner.firstChild.remove();
+    }
+}
+    
+function addToBanner(content) {
+    const banner = document.getElementById("banner");
+    const { itemName, imageURL, itemRank } = content;
+    const box = createBannerBox(itemName, imageURL, itemRank);
+    banner.appendChild(box);
+}
+
+function itemRoller(number) {
+
+    let rarity;
+
+    if (number == 3) {
+        rarity = itemData["threeStar"];
+    }
+    else if (number == 4) {
+        rarity = itemData["fourStar"];
+    }
+    else if (number == 5) {
+        rarity = itemData["fiveStar"];
+    }
+    else {
+        rarity = itemData["sixStar"];
+    }
+        
+    items = Object.keys(rarity);
+    randomItemIndex = Math.floor(Math.random() * items.length); 
+    const randomItem = rarity[items[randomItemIndex]];
+    return {
+        itemName: randomItem.itemName,
+        imageURL: randomItem.imageURL,
+        itemRank: randomItem.itemRank
+      };
+}
+
 function rollOne() { 
 
     //ticket system
     if (rollTickets >= 1) {
         rollTickets--;
         document.getElementById("header-tickets").innerHTML = rollTickets;
+        clearBanner();
         updatePity();
     }
     else {
@@ -169,20 +342,20 @@ function rollOne() {
     // photocontainer.style.display="block";
 }
 
-function rollTen() {
+function rollFive() {
     // ticket system
-    if (rollTickets >= 10){
-        rollTickets -= 10;
+    if (rollTickets >= 5){
+        rollTickets -= 5;
         document.getElementById("header-tickets").innerHTML = rollTickets;
+        clearBanner();
 
-        for(let i = 0; i < 10; i++){
+        for(let i = 0; i < 5; i++){
             updatePity();
         }
     }
     else{
         console.log("Insufficient Tickets");
-    }    
-
+    }
 
     //line of code na ito for reading the hidden div and revealing it 
     // const photocontainer = document.getElementById("photo-container");
@@ -196,22 +369,51 @@ function updatePity() {
     pullDetail();
 }
 
-function itemRoller(number) {
-    if (number == 3) {
-        rarity = itemData["⭐⭐⭐"];
-    }
-    else if (number == 4) {
-        rarity = itemData["⭐⭐⭐⭐"];
-    }
-    else if (number == 5) {
-        rarity = itemData["⭐⭐⭐⭐⭐"];
-    }
-    else {
-        rarity = itemData["⭐⭐⭐⭐⭐⭐"];
-    }
-    
-    items = Object.keys(rarity);
-    randomItemIndex = Math.floor(Math.random() * items.length); 
-    return rarity[items[randomItemIndex]].itemName;
+// For opening and closing modals (pop-ups)
+const modalHelp = document.getElementById("modal-help");
+const modalHelpBtn = document.getElementById("modal-help-btn");
+const modalOverlay = document.getElementById("modal-overlay");
 
+function showHelpModal() {
+    modalHelp.style.display = "block";
+    modalOverlay.style.display = "block";
 }
+
+function closeHelpModal() {
+    modalHelp.style.display = "none";
+    modalOverlay.style.display = "none";
+}
+
+modalOverlay.addEventListener('click', () => {
+    closeHelpModal();
+})
+
+// const openModalHelpButton = document.querySelectorAll('[data-open-help-button]');
+// const closeModalHelpButton = document.querySelectorAll('[data-close-help-button]');
+// const modalOverlay = document.getElementById("modal-overlay");
+
+// openModalHelpButton.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = document.querySelector(button.dataset.modalTarget);
+//         openModal(modal);
+//     })
+// })
+
+// closeModalHelpButton.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = document.closest('.modal-help')
+//         closeModal(modal);
+//     })
+// })
+
+// function openModal(modal) {
+//     if(modal == null) return
+//     modal.classList.add('active');
+//     modalOverlay.classList.add('active');
+// }
+
+// function openModal(modal) {
+//     if(modal == null) return
+//     modal.classList.remove('active');
+//     modalOverlay.classList.remove('active');
+// }
